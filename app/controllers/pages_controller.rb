ca9531275b87e319f98d2e1aed36e165d6c41998
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 15:23:21 +0000

    Integrated new and create actions for portfolios

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,12 +1,11 @@
 class PagesController < ApplicationController
   def home
-<<<<<<<<< saved version
+ saved version
 
-=========
     @post=blog.all
     @skills=skill.all
     @portfolios=portfolio.all
->>>>>>>>> local version
+ local version
   end
 
   def about
