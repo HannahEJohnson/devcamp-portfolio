4c469c624d9a88c61372f6f39d565b7963b7d428
Jordan Hudgens <jordan@devcamp.com>
2017-06-27 14:29:00 +0000

    created seed file for sample data

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,12 @@
 class PagesController < ApplicationController
   def home
+<<<<<<<<< saved version
+
+=========
+    @post=blog.all
+    @skills=skill.all
+    @portfolios=portfolio.all
+>>>>>>>>> local version
   end
 
   def about
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,24 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+10.times do |blog|
+  Blog.create!(
+    title: "My blog post #{blog}",
+    body: "aoihgdhgaoihhnnh hfgahf;g bfbg ;bg;hgr;h ori hgjkf ;badjh ;fkjgh kfjhd ;hga;fhga;uh g;ahgr;ieh"
+    )
+end
+puts "10 blog posts created"
+5.times do |skill|
+  Skill.create!(
+  title: "Rails #{skill}",
+  percent_utilized: "15"
+  )
+end
+puts "5 skills created"
+9.times do |portfolio_item|
+  Portfolio.create!(
+    title: "Portfolio",
+    subtitle: "the subtitle",
+    body: "gdiufdgihgihu iugbi i iisiudifuh ihduifs hifjkdhhffuh iu hhdsufh hfjk d bjdiufiu e wiu fisd fidf ksfiuifvisibubvi  iu bsiubdi  id bib sdub id isb fie dbsjvjksb v.",
+    main_image:"http://placehold.it/600x400",
+    thumb_image:"http://placehold.it/350x150"
+    )
+  end
+  puts "9 portfolio itmes created"
